c85f45cc73ad6fadbc4b4441a26a86f7052aa97d
Lorenzo Riano <lorenzo.riano@us.bosch.com>
2017-07-24 15:44:50 -0700

    fixing indents

--- mdn.py
+++ mdn.py
@@ -3,12 +3,13 @@ from keras.engine.topology import Layer
 import numpy as np
 import math
 
+
 def get_mixture_coef(output, numComonents=24, outputDim=1):
-    out_pi = output[:,:numComonents]
-    out_sigma = output[:,numComonents:2*numComonents]
-    out_mu = output[:,2*numComonents:]
+    out_pi = output[:, :numComonents]
+    out_sigma = output[:, numComonents:2 * numComonents]
+    out_mu = output[:, 2 * numComonents:]
     out_mu = K.reshape(out_mu, [-1, numComonents, outputDim])
-    out_mu = K.permute_dimensions(out_mu,[1,0,2])
+    out_mu = K.permute_dimensions(out_mu, [1, 0, 2])
     # use softmax to normalize pi into prob distribution
     max_pi = K.max(out_pi, axis=1, keepdims=True)
     out_pi = out_pi - max_pi
@@ -19,15 +20,17 @@ def get_mixture_coef(output, numComonents=24, outputDim=1):
     out_sigma = K.exp(out_sigma)
     return out_pi, out_sigma, out_mu
 
+
 def tf_normal(y, mu, sigma):
-    oneDivSqrtTwoPI = 1 / math.sqrt(2*math.pi)
+    oneDivSqrtTwoPI = 1 / math.sqrt(2 * math.pi)
     result = y - mu
-    result = K.permute_dimensions(result, [2,1,0])
+    result = K.permute_dimensions(result, [2, 1, 0])
     result = result * (1 / (sigma + 1e-8))
-    result = -K.square(result)/2
-    result = K.exp(result) * (1/(sigma + 1e-8))*oneDivSqrtTwoPI
+    result = -K.square(result) / 2
+    result = K.exp(result) * (1 / (sigma + 1e-8)) * oneDivSqrtTwoPI
     result = K.prod(result, axis=[0])
     return result
+
 
 def get_lossfunc(out_pi, out_sigma, out_mu, y):
     result = tf_normal(y, out_mu, out_sigma)
@@ -36,11 +39,14 @@ def get_lossfunc(out_pi, out_sigma, out_mu, y):
     result = -K.log(result + 1e-8)
     return K.mean(result)
 
+
 def mdn_loss(numComponents=24, outputDim=1):
     def loss(y, output):
         out_pi, out_sigma, out_mu = get_mixture_coef(output, numComponents, outputDim)
         return get_lossfunc(out_pi, out_sigma, out_mu, y)
+
     return loss
+
 
 class MixtureDensity(Layer):
     def __init__(self, kernelDim, numComponents, **kwargs):
@@ -51,17 +57,17 @@ class MixtureDensity(Layer):
 
     def build(self, inputShape):
         self.inputDim = inputShape[1]
-        self.outputDim = self.numComponents * (2+self.kernelDim)
-        self.Wh = K.variable(np.random.normal(scale=0.5,size=(self.inputDim, self.hiddenDim)))
-        self.bh = K.variable(np.random.normal(scale=0.5,size=(self.hiddenDim)))
-        self.Wo = K.variable(np.random.normal(scale=0.5,size=(self.hiddenDim, self.outputDim)))
-        self.bo = K.variable(np.random.normal(scale=0.5,size=(self.outputDim)))
+        self.outputDim = self.numComponents * (2 + self.kernelDim)
+        self.Wh = K.variable(np.random.normal(scale=0.5, size=(self.inputDim, self.hiddenDim)))
+        self.bh = K.variable(np.random.normal(scale=0.5, size=(self.hiddenDim)))
+        self.Wo = K.variable(np.random.normal(scale=0.5, size=(self.hiddenDim, self.outputDim)))
+        self.bo = K.variable(np.random.normal(scale=0.5, size=(self.outputDim)))
 
-        self.trainable_weights = [self.Wh,self.bh,self.Wo,self.bo]
+        self.trainable_weights = [self.Wh, self.bh, self.Wo, self.bo]
 
     def call(self, x, mask=None):
         hidden = K.tanh(K.dot(x, self.Wh) + self.bh)
-        output = K.dot(hidden,self.Wo) + self.bo
+        output = K.dot(hidden, self.Wo) + self.bo
         return output
 
     def get_output_shape_for(self, inputShape):
